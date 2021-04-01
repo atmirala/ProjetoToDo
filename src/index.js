aa36cb91ad2e7ad155e1e2e8df9c76e233733c94
atmirala <atmirala@gmail.com>
2021-04-01 03:43:55 -0300

    update to do put

--- src/index.js
+++ src/index.js
@@ -70,7 +70,23 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  const { title, deadline } = request.body;
+
+  const { id } = request.params;
+
+  const todo = users.todos.find(todo => todo.id ===id);
+
+  if (!todo) {
+    return response.status(404).json({error: "To Do not found"})
+  }
+
+  todo.title === title;
+
+  todo.deadLine = new Date(deadLine);
+
+  return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
